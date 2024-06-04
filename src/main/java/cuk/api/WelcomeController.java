3250cb95eccdf82f1a9649eaac5a64f0693d7475
1876070677 <fixerpremium@gmail.com>
2024-06-04 14:14:41 +0900

    Create project and init

---
+++ src/main/java/cuk/api/WelcomeController.java
@@ -0,0 +1,15 @@
+package cuk.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String welcome() {
+        return "Welcome to gajang~";
+    }
+}
