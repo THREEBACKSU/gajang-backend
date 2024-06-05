5fd22eee9937d3c663a729b771633266d0d78cd0
1876070677 <fixerpremium@gmail.com>
2024-06-05 20:47:16 +0900

    Add: User DB with docker-compose

---
+++ Database/initdb.d/create_table.sql
@@ -0,0 +1,24 @@
+create table gajang;
+use gajang;
+create table bank (
+    b_id int not null auto_increment,
+    name varchar(32) not null,
+    primary key(b_id)
+);
+create table address (
+    a_id int not null auto_increment,
+    name varchar(32) not null,
+    primary key(a_id)
+);
+create table user (
+    u_id varchar(32) not null,
+    password varchar(64) not null,
+    address_id int not null,
+    phone varchar(32) not null,
+    bank_id int nout null,
+    account_number varchar(64) default "",
+    rating float,
+    primary key(u_id),
+    foreign key(address_id) references address(a_id),
+    foreign key(bank_id) references bank(b_id)
+);
---
+++ Database/initdb.d/create_user.sql
@@ -0,0 +1,2 @@
+create user 'gajang'@'%' identified by 'gajang@123';
+grant all privileges on gajang.* to 'gajang'@'%';
