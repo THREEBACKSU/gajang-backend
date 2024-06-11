f4ad04aaba98ffe4d3bef964a6148a11f4b43f4f
KSH <1876070677@shbox.shop>
2024-06-12 02:14:51 +0900

    #1 Swagger BaseUrl Modifying

--- src/main/java/cuk/api/Swagger/SwaggerConfig.java
+++ src/main/java/cuk/api/Swagger/SwaggerConfig.java
@@ -10,9 +10,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebMvc
@@ -33,7 +36,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
     }
     @Bean
     public Docket api() {
+        Server publicServer = new Server("Public", "https://gajang.shbox.kr", "실제 api url", Collections.emptyList(), Collections.emptyList());
+        Server localServer = new Server("Local", "http://localhost:8080", "로컬 테스트", Collections.emptyList(), Collections.emptyList());
         return new Docket(DocumentationType.SWAGGER_2)
+                .servers(publicServer, localServer)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("cuk.api"))
                 .paths(PathSelectors.any())
--- src/main/java/cuk/api/User/UserController.java
+++ src/main/java/cuk/api/User/UserController.java
@@ -11,15 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Api(tags="사용자 기능")
 @Controller
+@RequestMapping("/api")
 public class UserController {
     private final UserService userService;
 
