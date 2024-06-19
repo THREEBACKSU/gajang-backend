4b5547063add5182e5de65802d4e8fd68c169ff6
1876070677 <fixerpremium@gmail.com>
2024-06-19 11:01:42 +0900

    Add: JsonLoginFilter

---
+++ src/main/java/cuk/api/User/UserDetailsServiceImpl.java
@@ -0,0 +1,14 @@
+package cuk.api.User;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String u_id) throws UsernameNotFoundException {
+        return null;
+    }
+}
